95ac1794cc4474bb6be6b23751f8224a6b1895b4
Tooclose21 <alasuper.as@gmail.com>
2023-10-15 20:21:08 +0200

    Javadoc added Ex2

--- Ex2.java
+++ Ex2.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Ex2 {
+    /**
+     * Program sprawdza poprawność znaków w łańcuchu względem łańcucha referencyjnego
+     * @param args command line args
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
