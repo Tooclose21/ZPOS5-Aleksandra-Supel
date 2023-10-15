c2936a365c417fdffd0530b2376ccbbd2b3679c2
Tooclose21 <alasuper.as@gmail.com>
2023-10-15 20:35:41 +0200

    Ex3 fixed: order in the print, line 26 changed s2 to s3

--- Ex3.java
+++ Ex3.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
 
 public class Ex3 {
+    /**
+     * Program zmienia kolor zapisany w systemie HEX na kolor zapisany w systemie RGB
+     * @param args command line args
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Podaj kod HEX: ");
         String k = scanner.nextLine();
-
+        if (k.length() != 6) {
+            throw new IllegalArgumentException("HEX musi mieć 6 znaków");
+        }
+        if (!hexCode(k)) {
+            throw new IllegalArgumentException("HEX może zawierać tylko litery i cyfry");
+        }
         scanner.close();
-//wyraz ma miec od 4 do 6 znakow?
         String s1 = k.substring(0, 2);
         String s2 = k.substring(2, 4);
         String s3 = k.substring(4, 6);
@@ -16,7 +24,17 @@ public class Ex3 {
         int r = Integer.parseInt(s1, 16);
         int g = Integer.parseInt(s2, 16);
         int b = Integer.parseInt(s3, 16);
-        System.out.printf("(" + r + "," + b + "," + g + ")");
+        System.out.printf("(" + r + "," + g + "," + b + ")");
 
+    }
+    private static boolean hexCode (String hex) {
+        String reference = "0123456789abcdefABCDEF";
+        for (int i = 0; i < hex.length(); i++) {
+            int id = reference.indexOf(hex.charAt(i));
+            if (id == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
