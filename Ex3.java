bf2e064bef41dd176cd545cbae9b3d10f5ddb7f8
Tooclose21 <alasuper.as@gmail.com>
2023-10-15 19:58:42 +0200

    Ex1 fixed: angle to radian and formulas errors

--- Ex3.java
+++ Ex3.java
@@ -8,14 +8,14 @@ public class Ex3 {
         String k = scanner.nextLine();
 
         scanner.close();
-
+//wyraz ma miec od 4 do 6 znakow?
         String s1 = k.substring(0, 2);
         String s2 = k.substring(2, 4);
         String s3 = k.substring(4, 6);
 
         int r = Integer.parseInt(s1, 16);
         int g = Integer.parseInt(s2, 16);
-        int b = Integer.parseInt(s2, 16);
+        int b = Integer.parseInt(s3, 16);
         System.out.printf("(" + r + "," + b + "," + g + ")");
 
     }
