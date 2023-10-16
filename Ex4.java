14e5639c917cb1ae8ae1a67f2cc5415b3e15d257
Tooclose21 <alasuper.as@gmail.com>
2023-10-16 20:48:28 +0200

    Added input exception handling and fixed errors (loop termination conditions)

--- Ex4.java
+++ Ex4.java
@@ -1,20 +1,26 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ex4 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        int n;
 
         System.out.print("Podaj liczbę całkowitą z przedziału [1,9]: ");
-        int n = scanner.nextInt();
+        try {
+            n = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("Nie podano liczby calkowitej.");
+        }
 
         scanner.close();
 
         if ((n > 9) || (n < 1)) {
             System.out.print("Podano liczbę spoza dopuszczalnego przedziału.");
         } else {
-            int[][] tabA = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 1; j <= n; j++) {
+            int[][] tabA = new int[n+1][n+1];
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j <= n; j++) {
                     tabA[i][j] = i * j;
                     System.out.print(tabA[i][j] + "\t");
                 }
